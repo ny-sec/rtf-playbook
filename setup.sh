8d80d2b68379997bbe3b97a903598415b77ac125
Tarik Abdel <31406864+ny-sec@users.noreply.github.com>
2018-05-26 09:55:04 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo su -
+apt-get update
+apt-get install python
+git clone https://github.com/trustedsec/ptf /opt/ptf
+cd /opt/ptf && ./ptf
+use modules/exploitation/install_update_all
+use modules/intelligence-gathering/install_update_all
+use modules/post-exploitation/install_update_all
+use modules/powershell/install_update_all
+use modules/vulnerability-analysis/install_update_all
+cd /pentest
